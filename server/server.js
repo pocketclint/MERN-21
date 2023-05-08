7020d7eca2d787e1227ae4cdb19629c7f8340c87
pocketclint <118079528+pocketclint@users.noreply.github.com>
2023-05-08 03:03:39 -0500

    server adjustments

--- server/server.js
+++ server/server.js
@@ -2,9 +2,17 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
+const { typeDefs, resolvers } = require('./schemas');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -13,9 +21,11 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
+const startApolloServer = async () => {
+  await server.start();
+  await server.listen(PORT);
+  server.applyMiddleware({ app });
+  console.log(`🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`);
+};
 
-app.use(routes);
-
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+startApolloServer();
